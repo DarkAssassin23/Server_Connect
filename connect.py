1ad96745284e2b42f7ca75acb3f63c6d067d6e6e
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2022-11-10 22:06:47 -0500

    added new file handling procedures and a new file type

--- connect.py
+++ connect.py
@@ -10,15 +10,25 @@ else:
 version = "3.0"
 whiteSpace = ' '
 connections = {}
-
 # Sets the directory of connect.py
 path = os.path.expanduser("~")+"/.ssh"
 
+connectionsFile = path+"/connections"
+deprecatedFile = False
+
 # If the file exists, it reads in all the connections
 def loadConnections():
+    global connectionsFile
+    global deprecatedFile
     try:
+        if(os.path.exists(connectionsFile+".dat")):
+            connectionsFile += ".dat"
+        elif(os.path.exists(connectionsFile+".txt")):
+            deprecatedFile = True
+            connectionsFile += ".txt"
+
         contents = []
-        with open(path+"/connections.txt","r") as f:
+        with open(connectionsFile,"r") as f:
             contents = f.readlines()
         for x in contents:
             # Legacy support for older connection.txt format, 
@@ -48,13 +58,40 @@ def loadConnections():
 # Saves all the connections to connections.txt
 # sorted by the name of the connection
 def saveConnections():
-    with open(path+"/connections.txt","w") as file:
-        for key,val in sorted(connections.items()):
-            # Legacy support for old connections.txt files
-            while(len(val)<3):
-                val.append("")
+    global deprecatedFile
+    global connectionsFile
+    try:
+        if(deprecatedFile):
+            print(connectionsFile)
+            connectionsFile = connectionsFile[:len(connectionsFile)-4]+".dat"
+        with open(connectionsFile+".tmp","w") as file:
+            for key,val in sorted(connections.items()):
+                # Legacy support for old connections.txt files
+                while(len(val)<3):
+                    val.append("")
 
-            file.write(key+"\0"+val[0]+"\0"+val[1]+"\0"+val[2]+"\n")
+                file.write(key+"\0"+val[0]+"\0"+val[1]+"\0"+val[2]+"\n")
+        os.rename(connectionsFile+".tmp", connectionsFile)
+    except:
+        print("An error occured saving your connections.\nAborting...")
+        exit()
+    if(deprecatedFile):
+        print("Your connections were read in from an older format with a deprecated filetype. "+
+            "Your connections have been automatically updated to the new standard and new file format. "+
+            "What would you like to do with the old file?")
+        print("1. Delete it")
+        print("2. Save it as a backup (default)")
+        try:
+            choice = int(input("Select an options: "))
+            if(choice == 1):
+                os.remove(connectionsFile[:len(connectionsFile)-4]+".txt")
+                print("Old file deleted")
+            else:
+                os.rename(connectionsFile[:len(connectionsFile)-4]+".txt", connectionsFile[:len(connectionsFile)-4]+".txt.bak")
+                print(f"Old connections file renamed to \'connections.txt.bak\'")
+        except:
+            os.rename(connectionsFile[:len(connectionsFile)-4]+".txt", connectionsFile[:len(connectionsFile)-4]+".txt.bak")
+            print(f"Old connections file renamed to \'connections.txt.bak\'")
 
 # Prints the help menu with all the commands
 # as well as what they do
